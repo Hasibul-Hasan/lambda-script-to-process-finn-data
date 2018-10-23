ee16be4f03352b4df90e2c8f201f5f505f17b910
Risul Islam <risul@regfire.no>
2018-10-23 13:41:53 +0200

    Implemented save to S3

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ process.env.PATH = `${process.env.PATH}:${process.env.LAMBDA_TASK_ROOT}`;
 const wkhtmltopdf = require("./utils/wkhtmltopdf");
 const errorUtil = require("./utils/error");
 
+const BUCKET = process.env.S3_BUCKET;
+const AWS = require("aws-sdk");
+const S3 = new AWS.S3({
+    signatureVersion: "v4"
+});
+
 const pdfBuffer = async (html, options, callback) => {
     try {
         // ["--page-size Letter", "--orientation Portrait", "--margin-bottom 1", "--margin-top 1", "--margin-right 1", "--margin-left 1"]
@@ -22,6 +28,25 @@ const pdfBuffer = async (html, options, callback) => {
     }
 };
 
+const uploadToS3 = async (buffer, key, callback) => {
+    try {
+        if (!buffer) {
+            callback(null, { statusCode: 500, body: '{ "message": "No PDF buffer available.", "error": true }' });
+            return;
+        }
+
+        const object = await S3.putObject({
+            Body: buffer,
+            Bucket: BUCKET,
+            Key: key
+        }).promise();
+
+        return object;
+    } catch (err) {
+        callback(err, { statusCode: 500, body: '{ "message": "Error occured while uploading PDF", "error": true }' });
+    }
+};
+
 exports.handler = async (event, context, callback) => {
     if (!event.html) {
         const errorResponse = errorUtil.createErrorResponse(400, "Validation error: Missing field 'html'.");
@@ -32,6 +57,11 @@ exports.handler = async (event, context, callback) => {
 
     try {
         const buffer = await pdfBuffer(event.html, event.options, callback);
+
+        // If we have been given a path to save to S3, we do
+        if (event.saveToPath) {
+            await uploadToS3(buffer, event.saveToPath, callback);
+        }
 
         callback(null, {
             data: buffer.toString("base64")

c5b3d82ac5d30f8c94bb7252bca5e78a4d4d8329
Risul Islam <risul@regfire.no>
2018-10-23 13:11:21 +0200

    Created a separate Await Async function to get the PDF buffer!

--- index.js
+++ index.js
@@ -3,7 +3,26 @@ process.env.PATH = `${process.env.PATH}:${process.env.LAMBDA_TASK_ROOT}`;
 const wkhtmltopdf = require("./utils/wkhtmltopdf");
 const errorUtil = require("./utils/error");
 
-exports.handler = function handler(event, context, callback) {
+const pdfBuffer = async (html, options, callback) => {
+    try {
+        // ["--page-size Letter", "--orientation Portrait", "--margin-bottom 1", "--margin-top 1", "--margin-right 1", "--margin-left 1"]
+        let pdfOptions = [];
+
+        if (options) {
+            pdfOptions = options;
+        } else {
+            pdfOptions = ["--margin-bottom 0", "--margin-right 0", "--margin-left 0"];
+        }
+
+        const pdfbuffer = await wkhtmltopdf(html, pdfOptions);
+
+        return pdfbuffer;
+    } catch (err) {
+        callback(err, { statusCode: 500, body: '{ "message": "Error occured while generating PDF", "error": true }' });
+    }
+};
+
+exports.handler = async (event, context, callback) => {
     if (!event.html) {
         const errorResponse = errorUtil.createErrorResponse(400, "Validation error: Missing field 'html'.");
 
@@ -11,21 +30,13 @@ exports.handler = function handler(event, context, callback) {
         return;
     }
 
-    // ["--page-size Letter", "--orientation Portrait", "--margin-bottom 1", "--margin-top 1", "--margin-right 1", "--margin-left 1"]
-    let pdfOptions = [];
+    try {
+        const buffer = await pdfBuffer(event.html, event.options, callback);
 
-    if (event.options) {
-        pdfOptions = event.options;
-    } else {
-        pdfOptions = ["--margin-bottom 0", "--margin-right 0", "--margin-left 0"];
-    }
-
-    wkhtmltopdf(event.html, pdfOptions)
-        .then(buffer => {
-            callback(null, {
-                data: buffer.toString("base64")
-            });
-        }).catch(error => {
-            callback(errorUtil.createErrorResponse(500, "Internal server error", error));
+        callback(null, {
+            data: buffer.toString("base64")
         });
+    } catch (error) {
+        callback(errorUtil.createErrorResponse(500, "Internal server error", error));
+    }
 };
